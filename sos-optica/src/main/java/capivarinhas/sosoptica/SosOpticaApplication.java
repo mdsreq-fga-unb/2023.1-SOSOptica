01b506287efd4c162b3df11302157adb421fffa8
TalesRG <talesrodriguesgoncalves98@gmail.com>
2023-05-02 22:11:01 -0300

    corrigindo padrao nome projeto

--- sos-optica/src/main/java/capivarinhas/sosoptica/SosOpticaApplication.java
+++ sos-optica/src/main/java/capivarinhas/sosoptica/SosOpticaApplication.java
@@ -1,4 +1,4 @@
-package capivarinhasdeplantao.sosoptica;
+package capivarinhas.sosoptica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- sos-optica/src/test/java/capivarinhas/sosoptica/SosOpticaApplicationTests.java
+++ sos-optica/src/test/java/capivarinhas/sosoptica/SosOpticaApplicationTests.java
@@ -1,4 +1,4 @@
-package capivarinhasdeplantao.sosoptica;
+package capivarinhas.sosoptica;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
