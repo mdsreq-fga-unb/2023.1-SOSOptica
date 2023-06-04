331720b4af04d244bda1562557673f0a4ce246ed
TalesRG <talesrodriguesgoncalves98@gmail.com>
2023-06-03 23:07:16 -0300

    Criando formulario para cadastro do cliente

--- sos-optica-app/src/view/CadastroCliente.js
+++ sos-optica-app/src/view/CadastroCliente.js
@@ -1,21 +1,23 @@
 import React from "react";
 import Card from "../components/Card";
 import FormGroup from "../components/form-group";
+
 class CadastroCliente extends React.Component{
 
 
     render() {
         return (
-            <div className="container" style={{position:"relative" ,left:"500px"}}>
+            <div className="container" style={{position:"relative" ,left:"600px"}}>
                 <Card title="Cadastrar Cliente">
                     <div className="row">
-                        <div className="col-md-12" >
-                            <FormGroup label="Nome * " htmlFor="inputDefault"  >
+                        <div className="col-md-3" >
+                            <FormGroup label="Nome * " htmlFor="inputDefault" >
                                 <input type="text"
                                        className="form-control"
                                        placeholder="Digite o nome do cliente"
                                        id="inputDefault"/>
                             </FormGroup>
+
 
                             <FormGroup label="CPF * " htmlFor="inputDefault">
                                 <input type="text"
@@ -30,17 +32,21 @@ class CadastroCliente extends React.Component{
                                        id="inputDefault"/>
                             </FormGroup>
 
+                        </div>
+                        <div className="col-md-3" >
                             <FormGroup label="Email * " htmlFor="inputDefault">
                                 <input type="email"
                                        className="form-control"
                                        id="inputDefault"/>
                             </FormGroup>
 
+
                             <FormGroup label="Data de nascimento * " htmlFor="inputDefault">
                                 <input type="date"
                                        className="form-control"
                                        id="inputDefault"/>
                             </FormGroup>
+
 
                             <FormGroup label="Genero" htmlFor="exampleSelect2" >
                                 <select multiple="" className="form-select" id="exampleSelect2" >
@@ -49,12 +55,13 @@ class CadastroCliente extends React.Component{
                                     <option>Feminino</option>
                                 </select>
                             </FormGroup>
-                            <br/>
-                            <button type="button" className="btn btn-primary">Cadastrar</button>
 
                         </div>
-                    </div>
 
+                    </div>
+                    <br/>
+                    <br/>
+                    <button type="button" className="btn btn-primary">Cadastrar</button>
                 </Card>
 
             </div>
