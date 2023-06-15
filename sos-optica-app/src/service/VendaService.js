4f394fdae8e12fb0b04c4d74a2c207e3d6d7f660
TalesRG <talesrodriguesgoncalves98@gmail.com>
2023-06-15 15:28:32 -0300

    fix/lint

--- sos-optica-app/src/service/VendaService.js
+++ sos-optica-app/src/service/VendaService.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Apiservice from "../app/apiservice";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 class VendaService extends Apiservice{
 
     constructor() {
         super("/api/vendas");
-
     }
 
     async salvarVendas(dadosVenda){
