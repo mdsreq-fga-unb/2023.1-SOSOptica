b2dc92ea226df6f4d8a1769b04c663e16cecf5d7
Osidious <gabriel73ren@gmail.com>
2023-06-27 13:06:56 -0300

    Cadastro Receita

--- sos-optica-app/src/view/cadastro/CadastroReceita.js
+++ sos-optica-app/src/view/cadastro/CadastroReceita.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "../../components/Card";
 import FormGroup from "../../components/form-group";
-import ReceitaService from "../../service/ReceitaService";
+//import ReceitaService from "../../service/ReceitaService";
 import {withRouter} from "react-router-dom";
 import {mensagemErro,mensagemSucesso} from "../../components/toastr";
 
