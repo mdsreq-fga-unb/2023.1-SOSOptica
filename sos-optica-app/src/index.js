88c3eb1abbb44995af7820a4b28748fb69adba8d
Osidious <gabriel73ren@gmail.com>
2023-07-11 00:08:56 +0000

    css mudanças

--- sos-optica-app/src/index.js
+++ sos-optica-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './main/App';
-import "./components/login/styles.css";
 
 ReactDOM.render(<App/>, document.getElementById('root'))
 
