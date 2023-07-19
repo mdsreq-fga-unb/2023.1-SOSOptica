01e92e0e9c139fc01091395f8fe15c4232bff5aa
TalesRG <talesrodriguesgoncalves98@gmail.com>
2023-07-19 17:36:27 -0300

    arrumando erros de lint

--- sos-optica-app/src/view/Consultar/TableVenda.js
+++ sos-optica-app/src/view/Consultar/TableVenda.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faEdit, faEye, faTrash} from '@fortawesome/free-solid-svg-icons';
+
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => {
