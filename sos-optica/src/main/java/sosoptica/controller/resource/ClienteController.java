93f0816aceadc5d7b1e01559535f60d8a671163d
TalesRG <talesrodriguesgoncalves98@gmail.com>
2023-07-20 01:15:00 -0300

    Arrumando data Cliente

--- sos-optica/src/main/java/sosoptica/controller/resource/ClienteController.java
+++ sos-optica/src/main/java/sosoptica/controller/resource/ClienteController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
-
+    //tudo ok por aqui
     private ClienteService clienteService;
 
     public ClienteController(ClienteService clienteService) {
